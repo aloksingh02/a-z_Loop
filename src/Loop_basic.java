d6c1c8cc1c469da681413032cfb7069b03f935f0
Asus <singhalokkumar427@gmail.com>
2025-05-08 13:04:13 +0530

    basic of loop

---
+++ src/Loop_basic.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Loop_basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        for(int i=1; i<=a; i++){
+            System.out.println("alok");
+        }
+    }
+}
