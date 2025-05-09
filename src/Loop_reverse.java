e4bed42df0e8f8e26f74eb164eba9e82618b3b08
Asus <singhalokkumar427@gmail.com>
2025-05-09 13:43:53 +0530

    arithmatic progression

---
+++ src/Loop_reverse.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Loop_reverse {
+    public static void main(String[] args) {
+        for(int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
